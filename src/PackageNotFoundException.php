300392285607444c2dee22d399e90cef1662437d
Daniel Foght Jensen <dalle@iflux.dk>
2018-08-31 00:37:46 +0200

    Adds a log of previously upacked zip files

---
+++ src/PackageNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+namespace iflux1990\scorm;
+
+use yii\base\ViewNotFoundException;
+
+/**
+ * Class FileNotFoundException
+ * @package iflux1990\scorm
+ * @version 1.0.0rc
+ */
+class FileNotFoundException extends ViewNotFoundException
+{
+
+}
